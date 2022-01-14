2118dde0b62cbf329237c6b8ced37e772f18da35
Christopher Bowers <c_bowers@me.com>
2022-01-14 16:51:30 -0500

    added some color options and made logo

--- client/src/components/GlobalStyle.js
+++ client/src/components/GlobalStyle.js
@@ -1,9 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+
   *, *::before, *::after {
     box-sizing: border-box;
   }
+
+  html {
+    --primary-background: hsla(258, 34%, 87%, 1);
+    --paradise-pink: hsl(339, 100%, 79%);
+    --primary-forground: hsla(252, 51%, 24%, 1);
+    --rich-black-fogra-39: hsla(156, 20%, 5%, 1);
+  }
+
   * {
     margin: 0;
   }
@@ -29,8 +38,10 @@ const GlobalStyle = createGlobalStyle`
   }
   body {
     font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
-    background-color: rgb(217, 210, 233);
+    background-color: var(--primary-background);
+    color: var(--paradise-pink);
   }
+
 `
 
 export default GlobalStyle
