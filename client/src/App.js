16055f3e1db16ce21a5c7cf95f6fd455dd24cea5
Christopher Bowers <c_bowers@me.com>
2022-01-14 12:36:32 -0500

    Added global styles component and cleaned up from CRA

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
+import { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import GlobalStyle from './components/GlobalStyle'
 
 function App() {
+
+  useEffect(() => {
+    document.title = 'drby'
+  }, [])
+
+
   return (
   <>
-
+    <GlobalStyle />
   </>
   )
 }
