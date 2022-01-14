4006da9e9250b0214be82ac0fd2b1b5467227b78
Cristen Negron <cristy.negron@gmail.com>
2022-01-14 09:14:58 -0500

    added models and migrations

---
+++ models/post.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Post.hasMany(models.Comment, { foreignKey: 'postId'})
+      Post.hasMany(models.Vote, { foreignKey: 'postId' })
+      Post.belongsTo(models.User, {foreignKey: 'userId'})
+      Post.belongsTo(models.Topic, { foreignKey: 'postId'})
+    }
+  };
+  Post.init({
+    title: DataTypes.STRING,
+    comment: DataTypes.STRING,
+    upvote: DataTypes.NUMBER,
+    downvote: DataTypes.NUMBER,
+    imgURL: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      onDelete: 'CASCADE'
+    }
+  }, {
+    sequelize,
+    modelName: 'Post',
+    tableName: 'posts'
+  });
+  return Post;
+};
