45f2c109fbbfb62b3a07e7195b0670364a9bc1ef
annika truong <annikatruongt@gmail.com>
2022-01-14 13:01:12 -0800

    made services folder. api client interceptor needs to be worked on. auth file done

---
+++ client/src/services/Auth.js
@@ -0,0 +1,31 @@
+import Client from './api'
+
+export const LogInUser = async (data) => {
+  try {
+    const res = await Client.post('/api/auth/login', data)
+    localStorage.setItem('token', res.data.token)
+    // Set the current signed in users token to localstorage
+    return res.data.user
+  } catch (error) {
+    throw error
+  }
+}
+
+export const RegisterUser = async (data) => {
+  try {
+    const res = await Client.post('/api/auth/register', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CheckSession = async () => {
+  try {
+    // Checks if the current token if it exists is valid
+    const res = await Client.get('/api/auth/session')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
