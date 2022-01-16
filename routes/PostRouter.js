0dddaa14d82e0478567c1b3a8dd7f595b6bddede
Christopher Bowers <c_bowers@me.com>
2022-01-16 17:46:46 +0000

    Added sequelize auth to register. Protected routes behind auth. Passord needs to be added to model for login to work.

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -1,13 +1,11 @@
 const Router = require('express').Router()
 const controller = require('../controllers/PostController')
+const middleware = require('../middleware')
 
-Router.get('/posts', controller.getAllPosts)
-Router.get('/posts/:id', controller.getPostById)
-
-Router.post('/posts', controller.createPost)
-
-Router.put('/posts/:id', controller.updatePost)
-
-Router.delete('/posts/:id', controller.deletePost)
+Router.get('/posts', middleware.stripToken, middleware.verifyToken, controller.getAllPosts)
+Router.get('/posts/:id', middleware.stripToken, middleware.verifyToken, controller.getPostById)
+Router.post('/posts', middleware.stripToken, middleware.verifyToken, controller.createPost)
+Router.put('/posts/:id', middleware.stripToken, middleware.verifyToken, controller.updatePost)
+Router.delete('/posts/:id', middleware.stripToken, middleware.verifyToken, controller.deletePost)
 
 module.exports = Router
