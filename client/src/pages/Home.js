f35d09eb8571c7ce886b24324b0f2eac1443495a
Christopher Bowers <c_bowers@me.com>
2022-01-16 17:24:03 -0500

    register and login set up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,10 @@
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
         <Wrapper>
           <h1>👋 from home</h1>
-          <p>
-          <Link to="/login">Login</Link>
-          </p>
         </Wrapper>
     )
 }
--- client/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import Axios from 'axios';
-
-export const BASE_URL = 'http://localhost:3001'
-const Client = Axios.create({ baseURL: BASE_URL })
-// intercept every request axios makes
-// Client.interceptors.request.use()
-
-export default Client
