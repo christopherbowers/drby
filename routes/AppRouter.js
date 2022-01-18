a6cda1e36b6eaf3ee2d338c77a6d8bcb7f51925a
chriszc97 <czeas1997@gmail.com>
2022-01-18 08:16:47 -0500

    routes and contoller complete

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,6 +1,8 @@
 const Router = require('express').Router();
 const UserRouter = require('./UserRouter');
+const CommentRouter = require('./CommentRouter');
 
 Router.use('/users', UserRouter);
+Router.use('/comments', CommentRouter);
 
 module.exports = Router;
---
+++ routes/CommentRouter.js
@@ -0,0 +1,8 @@
+const commentRouter = require('express').Router();
+const commentController = require('../controllers/CommentControllers');
+
+commentRouter.post('/', commentController.createComment);
+commentRouter.put('/:comment_id', commentController.updateComment);
+commentRouter.delete('/:comment_id', commentController.deleteComment);
+
+module.exports = commentRouter;
