92aaa756aed87ea5821796e0a75538ec436bb712
chriszc97 <czeas1997@gmail.com>
2022-01-18 15:48:02 -0500

    comments controller

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -3,10 +3,12 @@ const AuthRouter = require('./AuthRouter');
 const UserRouter = require('./UserRouter');
 const TopicRouter = require('./TopicRouter');
 const PostRouter = require('./PostRouter');
+const CommentRouter = require('./CommentRouter');
 
 Router.use('/auth', AuthRouter);
 Router.use('/users', UserRouter);
 Router.use('/topics', TopicRouter);
 Router.use('/posts', PostRouter);
+Router.use('/comments', CommentRouter);
 
 module.exports = Router;
