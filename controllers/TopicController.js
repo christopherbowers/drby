28511204aabbcba714e44b52238536358c996b3d
chriszc97 <czeas1997@gmail.com>
2022-01-19 10:13:41 -0500

    JointTopicAndPost

--- controllers/TopicController.js
+++ controllers/TopicController.js
@@ -1,26 +1,37 @@
-const { Topic } = require ('../models')
+const { Topic, Post } = require('../models');
 
 const getAllTopics = async (req, res) => {
   try {
-    const topics = await Topic.findAll()
-    res.send(topics)
+    const topics = await Topic.findAll();
+    res.send(topics);
   } catch (error) {
     throw error;
   }
-}
+};
 
 const getTopicById = async (req, res) => {
   try {
-    let id = req.params.id
-    const topic = await Topic.findByPk(id)
-    res.send(topic)
+    let id = req.params.id;
+    const topic = await Topic.findByPk(id);
+    res.send(topic);
   } catch (error) {
-    throw error
+    throw error;
   }
-}
-
+};
+const getTopicPosts = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const posts = await Post.findAll({
+      where: { topicId: id }
+    });
+    res.send(posts);
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
   getAllTopics,
-  getTopicById
-}
+  getTopicById,
+  getTopicPosts
+};
