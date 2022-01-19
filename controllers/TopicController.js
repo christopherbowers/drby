78b1262eb50e9221821445f786c0cfacc9eacf62
chriszc97 <czeas1997@gmail.com>
2022-01-19 13:50:24 -0500

    topics/posts

--- controllers/TopicController.js
+++ controllers/TopicController.js
@@ -15,6 +15,13 @@ const getThreePosts = async (req, res) => {
     const posts = await Post.findAll({
       where: { topicId: id },
       limit: 3,
+      include: [
+        {
+          model: Topic,
+          required: true,
+          attributes: ['name']
+        }
+      ],
       order: sequelize.random()
     });
     res.send(posts);
@@ -26,7 +33,15 @@ const getTopicPosts = async (req, res) => {
   try {
     let id = req.params.id;
     const posts = await Post.findAll({
-      where: { topicId: id }
+      where: { topicId: id },
+      attributes: ['title', 'postbody'],
+      include: [
+        {
+          model: Topic,
+          required: true,
+          attributes: ['name']
+        }
+      ]
     });
     res.send(posts);
   } catch (error) {
