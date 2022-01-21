92e39abb6f555e84f26841274e7d372d521c1387
Christopher Bowers <c_bowers@me.com>
2022-01-21 09:16:47 -0500

    User name big

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,7 +1,14 @@
+import styled from 'styled-components'
+
 export default function User({authedUser}) {
   return (
-    <>
+    <Wrapper>
       <h1>👋 {authedUser.firstName}</h1>
-    </>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  text-align: center;
+  font-size: 3rem;
+`
