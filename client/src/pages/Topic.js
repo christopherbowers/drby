6c652bcd6df752e028273b51bd5b93479a32c14d
Christopher Bowers <c_bowers@me.com>
2022-01-20 22:02:51 -0500

    Migration fix

--- client/src/pages/Topic.js
+++ client/src/pages/Topic.js
@@ -15,8 +15,6 @@ export default function Topic() {
 
   const [topicTitle, setTopicTitle] = useState()
 
-  console.log(BASE_URL)
-
   const getTopicPosts = () => {
     axios.get(`${BASE_URL}/topics/${topicId}`)
     .then( res => {
